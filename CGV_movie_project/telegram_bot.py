df364cb15912f9e6886413e62a802c6307eeb3ba
queengodqueen <52725917+queengodqueen@users.noreply.github.com>
2019-07-22 14:39:08 +0900

    Delete telegram_bot.py

--- CGV_movie_project/telegram_bot.py
+++
@@ -1,8 +0,0 @@
-import telegram
-
-bot = telegram.Bot(token = '803288229:AAF3jcyErpuDmvg2Ga9yDaZ9Djd0_9tv7vE')
-
-#for i in bot.getUpdates():
-#   print(i.message)
-
-bot.sendMessage(chat_id=705760272, text = "Welcome!")
